cfceac10d894e031162ea271a307838447165aed
Mester Zsolt <zsolt.mester67@gmail.com>
2016-06-02 08:37:40 +0200

    A network-speed-test fix.

--- network-speed-test.sh
+++ network-speed-test.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
-date >> /home/zsmester/log/network-speed.log
-speedtest-cli >> /home/zsmester/log/network-speed.log
-echo "" >> /home/zsmester/log/network-speed.log
+date=$(date)
+
+result=$(/usr/local/bin//speedtest-cli --simple)
+ping=$(echo "$result" | grep Ping | cut -d ":" -f 2 | xargs)
+download=$(echo "$result" | grep Download | cut -d ":" -f 2 | xargs)
+upload=$(echo "$result" | grep Upload | cut -d ":" -f 2 | xargs)
+
+echo "$date\t$ping\t$download\t$upload" >> /home/zsmester/log/network-speed.log
